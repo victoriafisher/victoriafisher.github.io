bfd2d2f7a87838d8a59f12b9b29b7a12312c2f4a
victoriafisher <vafis1@student.monash.edu>
2018-05-25 15:30:33 +1000

    upload

---
+++ 4_reader/assets/js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+
+  $(".meme1").hover(
+    function(){
+      var number = Math.round(Math.random() * 4);
+        $(".meme_img_wrapper").append(
+          "<img class='meme_img' src='assets/images/article1/" + number + ".jpg' />"
+        )
+    },
+    function(){
+      $(".meme_img").remove();
+    }
+  );
+
+  $(".meme2").hover(
+    function(){
+      var number = Math.round(Math.random() * 8);
+        $(".meme_img_wrapper").append(
+          "<img class='meme_img' src='assets/images/article2/" + number + ".jpg' />"
+        )
+    },
+    function(){
+      $(".meme_img").remove();
+    }
+  );
+
+  $(".meme3").hover(
+    function(){
+      var number = Math.round(Math.random() * 8);
+        $(".meme_img_wrapper").append(
+          "<img class='meme_img' src='assets/images/article3/" + number + ".jpg' />"
+        )
+    },
+    function(){
+      $(".meme_img").remove();
+    }
+  );
+});
